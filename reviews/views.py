bfb37b792bbc2ebc6d615e40d1874ff4893cbf90
EtienneRQT <etienne.riquet@sfr.fr>
2021-09-07 19:25:03 +0200

    added new view

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -23,6 +24,27 @@ class ReviewView(View):
     def post(self, request):
         form = ReviewForm(request.POST)
 
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+        return render(request, "reviews/review.html", {"form": form})
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
